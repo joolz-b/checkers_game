9d2dcb514995b4ebd6e82442fab0e557237d2784
joolz-b <s3690935@student.rmit.edu.au>
2021-05-29 21:12:18 +1000

    Bootstrap

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = 'super secret key :)'
+app.config['SESSION_TYPE'] = 'filesystem'
 Bootstrap(app)
+
 
 @app.route('/')
 def index():
@@ -11,6 +14,18 @@ def index():
 @app.route('/game')
 def game():
    return render_template('game.html')
+
+@app.route('/tutorial')
+def tutorial():
+   return render_template('tutorial.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+   return render_template('signup.html')
 
 if __name__ == '__main__':
    app.debug = True
