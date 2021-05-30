f5b6d2c3902e21c3f797a1a6d8bcdc0269990ca2
joolz-b <s3690935@student.rmit.edu.au>
2021-05-30 19:34:39 +1000

    Skeleton login/signup page

--- src/__init__.py
+++ src/__init__.py
@@ -1,31 +1,57 @@
-from flask import Flask, render_template, session
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, session, redirect, url_for, request
+from userUtil import is_user_logged_in, create_user, authenticate_user
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'super secret key :)'
 app.config['SESSION_TYPE'] = 'filesystem'
-Bootstrap(app)
-
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
+# displays the actual game board
 @app.route('/game')
 def game():
-   return render_template('game.html')
+   
+   if is_user_logged_in():
+      return render_template('game.html')
 
+   else:
+      return redirect(url_for('login'))
+      
+# information on how to play the game
 @app.route('/tutorial')
 def tutorial():
    return render_template('tutorial.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-   return render_template('login.html')
+   
+   if request.method == 'GET':
+      return render_template('login.html')
 
-@app.route('/signup')
+   elif request.method == 'POST':
+
+      # validate and check if the user exists
+      if authenticate_user(request.form['username'], request.form['password']):
+         return redirect(url_for('game'))
+
+      # todo: if it doesn't exist, redirect back to login page with errors
+      else:
+         return redirect(url_for('login'))
+
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
-   return render_template('signup.html')
+
+   if request.method == 'GET':
+      return render_template('signup.html')
+
+   elif request.method == 'POST':
+
+      # todo: validate and check if user exists, if not, create the user
+      if request.form['password'] == request.form['password_confirm']:
+         create_user(request.form['username'], request.form['password'])
+
 
 if __name__ == '__main__':
    app.debug = True
---
+++ src/userUtil.py
@@ -0,0 +1,26 @@
+from flask import session
+
+def is_user_logged_in():
+  res = False
+
+  if session.get('username') != None:
+    res = True
+
+  return res
+
+def authenticate_user(username, password):
+
+  res = False
+  # todo
+  if True:
+    session['username'] = username
+    res = True
+
+  return res
+
+def create_user(username):
+  # todo
+  return
+
+def logout():
+  session.clear()
